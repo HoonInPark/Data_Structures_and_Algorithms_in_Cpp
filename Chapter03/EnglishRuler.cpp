15ceb98397680149c71e3c244119973d55d7366a
LeeChangJoonJoonJoon <hooninpark@e8ight.co.kr>
2025-03-01 09:39:07 +0900

    linked list must be better

--- Chapter03/EnglishRuler.cpp
+++ Chapter03/EnglishRuler.cpp
@@ -3,43 +3,10 @@ using namespace std;
 
 void InsertTickElem(vector<int>& _InTicks, int _InOffsetIdx, int _InMajorTickLen)
 {
-    /*
-    4 1 2 1 3 1 2 1 4
-    */
-    const int&& RightOffsetIdx = _InOffsetIdx + pow(2, _InMajorTickLen - 1);
-
-    if (_InTicks[_InOffsetIdx] < 1) _InTicks[_InOffsetIdx] = _InMajorTickLen;
-    if (_InTicks[RightOffsetIdx] < 1) _InTicks[RightOffsetIdx] = _InMajorTickLen;
-
-    if (_InMajorTickLen < 2) return;
-    
-    InsertTickElem(_InTicks, _InOffsetIdx, _InMajorTickLen - 1);
-    InsertTickElem(_InTicks, _InOffsetIdx + RightOffsetIdx / 2, _InMajorTickLen - 1);
 }
 
 void EnglishRuler(int _InMajorTickLen)
 {
-    const int&& TicksSize = pow(2, _InMajorTickLen - 1) + 1;
-    vector<int> Ticks(TicksSize, 0);
-
-    InsertTickElem(Ticks, 0, _InMajorTickLen);
-    
-    for (auto& elem : Ticks)
-    {
-        cout << elem << endl;
-    }
-
-#if 0
-    for (int i = 0; i < TicksSize; i++)
-    {
-        for (int i = 0; i < Ticks[i]; i++)
-        {
-            cout << '-';
-        }
-
-        cout << '\n' << endl;
-    }
-#endif
 }
 
 int main()
