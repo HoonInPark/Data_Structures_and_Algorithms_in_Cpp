15e0637a6511495d5cf7b8e648b03825a1a99a64
LeeChangJoonJoonJoon <hooninpark@e8ight.co.kr>
2025-03-04 11:33:19 +0900

    made insert func

--- Chapter03/DoublyLinkedList.h
+++ Chapter03/DoublyLinkedList.h
@@ -6,13 +6,13 @@ class Node
 public:
     Node(const T& _InElem, Node* _pInPrev, Node* _pInNext);
     ~Node();
-    
+
 private:
     const T* m_pElem;
 
-    Node* m_pPrev;
-    Node* m_pNext;
-    
+    Node* m_pPrev { nullptr };
+    Node* m_pNext { nullptr };
+
     friend DounblyLinkedList;
 };
 
@@ -25,9 +25,18 @@ public:
     DoublyLinkedList();
     ~DoublyLinkedList();
 
-    void Add(
-        const T& _InElem, 
-        Node<T>* _pOutPrev = nullptr, 
+#if 0
+    void InsertBefore(
+        Node<T>* _pInPrev,
+        const T& _InElem,
+        Node<T>* _pOutPrev = nullptr,
+        Node<T>* _pOutNext = nullptr);
+#endif // activate if it needed
+
+    void InsertAfter(
+        Node<T>* _pInNext,
+        const T& _InElem,
+        Node<T>* _pOutPrev = nullptr,
         Node<T>* _pOutNext = nullptr);
 
 private:
--- Chapter03/EnglishRuler.cpp
+++ Chapter03/EnglishRuler.cpp
@@ -4,21 +4,18 @@ using namespace std;
 
 void InsertTickElem(DoublyLinkedList<int>& _InTicks, int _InMajorTickLen, _List_iterator<int> _InIter)
 {
-    
+
 }
 
 void EnglishRuler(int _InMajorTickLen)
 {
-    DoublyLinkedList<int> Ticks;
-    
 
-    // InsertTickElem(Ticks, _InMajorTickLen, Ticks.end());
 }
 
 int main()
 {
     int nInput;
     cin >> nInput;
-    
+
     EnglishRuler(nInput);
 }
