21c00878175a7875ab976d8251483d09ae627421
LeeChangJoonJoonJoon <hooninpark@e8ight.co.kr>
2025-03-06 13:31:48 +0900

    WithoutRecursion impl for simplification

--- Chapter03/LinFib.cpp
+++ Chapter03/LinFib.cpp
@@ -1,29 +1,31 @@
 #include "bits/stdc++.h"
 using namespace std;
 
-#if 0
+
+void WithoutRecursion(int _InFibIdx)
+{
+    int Former = 0;
+    int Later = 1;
+
+    for (int i = 0; i < _InFibIdx + 1; i++)
+    {
+        int NumTmp = Later;
+        Later = Former + Later;
+        Former = NumTmp;
+    }
+
+    cout << Later << endl;
+}
+
 // 선형재귀로 피보나치 구현하기
 int LinFib(int _InFibIdx)
 {
     if (_InFibIdx < 2) return 1;
 
-
-}
-#endif
-
-void WithoutRecurse(int _InFibIdx)
-{
-    int Former = 0;
-    int Later = 1;
-
-    for (int i = 0; i < _InFibIdx; i++)
-    {
-        Later = Former + Later;
-        
-    }
+    
 }
 
 int main()
 {
-    
+    WithoutRecursion(5);
 }
