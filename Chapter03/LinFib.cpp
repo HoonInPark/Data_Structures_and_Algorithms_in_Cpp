614b88daab2b05fd569d60089c4a1413d54baee9
LeeChangJoonJoonJoon <hooninpark@e8ight.co.kr>
2025-03-06 15:25:36 +0900

    make LinFib fancier

--- Chapter03/LinFib.cpp
+++ Chapter03/LinFib.cpp
@@ -19,20 +19,17 @@ void WithoutRecursion(int _InFibIdx)
 }
 
 // 선형재귀로 피보나치 구현하기
-int LinFib(int _InFormer, int _InLater, int _InIdx)
+int LinFib(int _InIdx, int _InFormer = 0, int _InLater = 1)
 {
     if (_InIdx < 1)
         return _InFormer + _InLater;
 
-    return LinFib(_InLater, _InFormer + _InLater, _InIdx - 1);
+    return LinFib(_InIdx - 1, _InLater, _InFormer + _InLater);
 }
 
 int main()
 {
     // WithoutRecursion(8);
 
-    int Former = 0;
-    int Later = 1;
-
-    cout << LinFib(0, 1, 4) << endl;
+    cout << LinFib(5) << endl;
 }
