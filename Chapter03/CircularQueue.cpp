10b65c5ff2203d349d19487c8c688c3efdcfa2b2
LeeChangJoonJoonJoon <hooninpark@e8ight.co.kr>
2025-03-07 17:09:04 +0900

    func Size and IsEmpty impl

--- Chapter03/CircularQueue.cpp
+++ Chapter03/CircularQueue.cpp
@@ -2,18 +2,37 @@
 using namespace std;
 
 #define CIR_Q_WITH_CIR_ARR 1
+#define ARR_SIZE 100
 
 #if CIR_Q_WITH_CIR_ARR
 template<typename T>
 class CircularQueue
 {
 public:
+    CircularQueue(int _InArrSize = ARR_SIZE)
+    {
+        m_ArrSize = _InArrSize;
+        m_CirArr = new T[_InArrSize];
+    }
+    ~CircularQueue()
+    {
+        delete[] m_CirArr;
+    }
+
     bool Enqueue(const T& _InElem);
     bool Dequeue();
     T& Front();
 
-private:
+    inline int Size() const { return m_ArrSize; }
+    inline bool IsEmpty() const { return m_ArrSize == 0 ? true : false; }
 
+private:
+    T* m_CirArr;
+
+    int m_ArrSize;
+
+    int m_IdxBack{ 0 };
+    int m_IdxFront{ 0 };
 };
 #endif
 
