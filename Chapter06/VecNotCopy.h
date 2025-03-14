13c43bb36b1a9164677e18ea7a40ecee545d1b63
LeeChangJoonJoonJoon <hooninpark@e8ight.co.kr>
2025-03-14 15:31:35 +0900

    configuring general adt push back cal

--- Chapter06/VecNotCopy.h
+++ Chapter06/VecNotCopy.h
@@ -2,34 +2,48 @@
 using namespace std;
 
 /*
- * 
- * to avoid copying all the elem in the prev arr for adding new elem over prev cap of arr, 
+ *
+ * theme : FUCK std::vector
+ * to avoid copying all the elem in the prev arr for adding new elem over prev cap of arr,
  * VecNotCopy leaves one last elem (which type is ptr) for make it an 'entrance' for another arr.
-*/
+ */
 
-template<typename T>
+template <typename T>
 class VecNotCopy
 {
 public:
-    VecNotCopy(int _InCap = 0);
+    VecNotCopy(int _InInitCap = 20);
+    VecNotCopy(const VecNotCopy& _Rhs);
     ~VecNotCopy();
 
     inline int GetSize() const { return m_Size; }
     inline int GetCap() const { return m_Cap; }
     inline T& operator[](int _InIdx) { return *m_arrData[_InIdx]; }
 
-    bool Add(int _InIdx);
+    bool Reserve(int _InCap);
+
+    bool PushBack(const T& _InElem);
     bool Remove(int _InIdx);
 
 private:
     T** m_arrData;
 
-    int m_Size;
-    int m_Cap;
+    const int m_Cap;
+    int m_Size{ 0 };
 };
 
 template <typename T>
-inline VecNotCopy<T>::VecNotCopy(int _InCap)
+inline VecNotCopy<T>::VecNotCopy(int _InInitCap)
+    : m_Cap(_InInitCap)
+{
+    m_arrData = new T * [_InInitCap];
+
+    for (auto& Elem : m_arrData)
+        Elem = nullptr;
+}
+
+template<typename T>
+inline VecNotCopy<T>::VecNotCopy(const VecNotCopy& _Rhs)
 {
 }
 
@@ -38,10 +52,28 @@ inline VecNotCopy<T>::~VecNotCopy()
 {
 }
 
-template <typename T>
-inline bool VecNotCopy<T>::Add(int _InIdx)
+template<typename T>
+inline bool VecNotCopy<T>::Reserve(int _InCap)
 {
-    return false;
+    if (_InCap)
+        return false;
+}
+
+template <typename T>
+bool VecNotCopy<T>::PushBack(const T& _InElem)
+{
+    if (m_Size < m_Cap)
+    {
+
+
+        m_Size++;
+        return true;
+    }
+    else
+    {
+        /* alloc another arr code */
+        return true;
+    }
 }
 
 template <typename T>
